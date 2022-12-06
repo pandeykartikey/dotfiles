9d69758df4e94a4467332ff619c3d12b3a68a516
Kartikey Pandey <pandeykartikey99@gmail.com>
2022-12-06 10:46:17 +0530

    Update .hyper.js

--- Hyper/.hyper.js
+++ Hyper/.hyper.js
@@ -92,6 +92,12 @@ module.exports = {
         //
         // Cygwin
         // - Example: `C:\\cygwin64\\bin\\bash.exe`
+        navigation: {
+            up: 'ctrl+alt+up',
+            down: 'ctrl+alt+down',
+            left: 'ctrl+alt+left',
+            right: 'ctrl+alt+right'
+          },
         shell: '',
         // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
         // by default `['--login']` will be used
